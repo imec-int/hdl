e5050471b887ed001c61be939197cc8adca7401b
wouter Devriese <devriese.wouter@gmail.com>
2020-11-13 14:17:05 +0100

    adapted files for less lanes, TBVerified

--- projects/trenz/common/adc_bd.tcl
+++ projects/trenz/common/adc_bd.tcl
@@ -1,4 +1,3 @@
-
 source $ad_hdl_dir/library/jesd204/scripts/jesd204.tcl
 
 # TX parameters
@@ -19,18 +18,47 @@ set RX_NUM_OF_CONVERTERS 8 ; # M
 set RX_SAMPLES_PER_FRAME 1 ; # S
 set RX_SAMPLE_WIDTH 16     ; # N/NP
 
-set RX_SAMPLES_PER_CHANNEL [expr $RX_NUM_OF_LANES * 32.0 / \
-                                ($RX_NUM_OF_CONVERTERS * $RX_SAMPLE_WIDTH)] ; # L * 32 / (M * N)
+# set RX_SAMPLES_PER_CHANNEL [expr $RX_NUM_OF_LANES * 32.0 /  ($RX_NUM_OF_CONVERTERS * $RX_SAMPLE_WIDTH)] ; # L * 32 / (M * N) only works if F<8
+
+set LINK_LAYER_BYTES_PER_BEAT 4
+
+# F = (M * N * S) / (L * 8)
+set RX_BYTES_PER_FRAME [expr ($RX_NUM_OF_CONVERTERS * $RX_SAMPLE_WIDTH * $RX_SAMPLES_PER_FRAME) / ($RX_NUM_OF_LANES * 8)];
+# one beat per lane must accommodate at least one frame
+set RX_TPL_BYTES_PER_BEAT [expr max($RX_BYTES_PER_FRAME, $LINK_LAYER_BYTES_PER_BEAT)]
+
+# datapath width = L * 8 * TPL_BYTES_PER_BEAT / (M * N)
+set RX_SAMPLES_PER_CHANNEL [expr ($RX_NUM_OF_LANES * 8 * $RX_TPL_BYTES_PER_BEAT) / ($RX_NUM_OF_CONVERTERS * $RX_SAMPLE_WIDTH)]
+
 
 set adc_fifo_name axi_ad9675_fifo
 set adc_data_width [expr $RX_SAMPLE_WIDTH * $RX_NUM_OF_CONVERTERS * $RX_SAMPLES_PER_CHANNEL]
 
 # adc peripherals
+ad_ip_instance axi_clkgen axi_ad9675_rx_clkgen
+ad_ip_parameter axi_ad9675_rx_clkgen CONFIG.ID 2
+ad_ip_parameter axi_ad9675_rx_clkgen CONFIG.CLKIN_PERIOD 4
+ad_ip_parameter axi_ad9675_rx_clkgen CONFIG.VCO_DIV 1
+ad_ip_parameter axi_ad9675_rx_clkgen CONFIG.VCO_MUL 4
+ad_ip_parameter axi_ad9675_rx_clkgen CONFIG.CLK0_DIV 4
+# When F = 8 add a second clock to drive the transport layer with a 2x slower clock
+if {$RX_TPL_BYTES_PER_BEAT > $LINK_LAYER_BYTES_PER_BEAT} {
+  ad_ip_parameter axi_ad9675_rx_clkgen CONFIG.ENABLE_CLKOUT1 1
+  ad_ip_parameter axi_ad9675_rx_clkgen CONFIG.CLK1_DIV 8
+  set rx_link_clk axi_ad9675_rx_clkgen/clk_0
+  set rx_data_clk axi_ad9675_rx_clkgen/clk_1
+} else {
+  set rx_link_clk axi_ad9675_rx_clkgen/clk_0
+  set rx_data_clk axi_ad9675_rx_clkgen/clk_0
+}
 
 ad_ip_instance axi_adxcvr axi_ad9675_xcvr
 ad_ip_parameter axi_ad9675_xcvr CONFIG.NUM_OF_LANES $RX_NUM_OF_LANES
 ad_ip_parameter axi_ad9675_xcvr CONFIG.QPLL_ENABLE 0
 ad_ip_parameter axi_ad9675_xcvr CONFIG.TX_OR_RX_N 0
+ad_ip_parameter axi_ad9675_xcvr CONFIG.SYS_CLK_SEL 0
+ad_ip_parameter axi_ad9675_xcvr CONFIG.OUT_CLK_SEL 3
+
 
 adi_axi_jesd204_rx_create axi_ad9675_jesd $RX_NUM_OF_LANES
 
@@ -39,11 +67,11 @@ adi_tpl_jesd204_rx_create axi_ad9675_tpl_core $RX_NUM_OF_LANES \
                                                $RX_SAMPLES_PER_FRAME \
                                                $RX_SAMPLE_WIDTH
 
-ad_ip_instance util_cpack2 axi_ad9675_cpack [list \
-  NUM_OF_CHANNELS $RX_NUM_OF_CONVERTERS \
-  SAMPLES_PER_CHANNEL $RX_SAMPLES_PER_CHANNEL \
-  SAMPLE_DATA_WIDTH $RX_SAMPLE_WIDTH \
-  ]
+#ad_ip_instance util_cpack2 axi_ad9675_cpack [list \
+#  NUM_OF_CHANNELS $RX_NUM_OF_CONVERTERS \
+#  SAMPLES_PER_CHANNEL $RX_SAMPLES_PER_CHANNEL \
+#  SAMPLE_DATA_WIDTH $RX_SAMPLE_WIDTH \
+#  ]
 
 ad_ip_instance axi_dmac axi_ad9675_dma
 ad_ip_parameter axi_ad9675_dma CONFIG.DMA_TYPE_SRC 1
@@ -55,7 +83,7 @@ ad_ip_parameter axi_ad9675_dma CONFIG.SYNC_TRANSFER_START 0
 ad_ip_parameter axi_ad9675_dma CONFIG.DMA_LENGTH_WIDTH 24
 ad_ip_parameter axi_ad9675_dma CONFIG.DMA_2D_TRANSFER 0
 ad_ip_parameter axi_ad9675_dma CONFIG.CYCLIC 0
-ad_ip_parameter axi_ad9675_dma CONFIG.DMA_DATA_WIDTH_SRC $adc_data_width
+ad_ip_parameter axi_ad9675_dma CONFIG.DMA_DATA_WIDTH_SRC [expr $RX_SAMPLE_WIDTH*$RX_SAMPLES_PER_CHANNEL*$RX_NUM_OF_CONVERTERS]
 ad_ip_parameter axi_ad9675_dma CONFIG.DMA_DATA_WIDTH_DEST 64
 
 if {$sys_zynq == 0 || $sys_zynq == 1} {
@@ -64,60 +92,133 @@ if {$sys_zynq == 0 || $sys_zynq == 1} {
 
 # shared transceiver core
 
-ad_ip_instance util_adxcvr util_daq3_xcvr
-ad_ip_parameter util_daq3_xcvr CONFIG.RX_NUM_OF_LANES $RX_NUM_OF_LANES
-ad_ip_parameter util_daq3_xcvr CONFIG.TX_NUM_OF_LANES $TX_NUM_OF_LANES
-ad_ip_parameter util_daq3_xcvr CONFIG.QPLL_REFCLK_DIV 1
-ad_ip_parameter util_daq3_xcvr CONFIG.QPLL_FBDIV_RATIO 1
-ad_ip_parameter util_daq3_xcvr CONFIG.QPLL_FBDIV 0x30; # 20
-ad_ip_parameter util_daq3_xcvr CONFIG.RX_OUT_DIV 1
-ad_ip_parameter util_daq3_xcvr CONFIG.TX_OUT_DIV 1
-ad_ip_parameter util_daq3_xcvr CONFIG.RX_DFE_LPM_CFG 0x0904
-ad_ip_parameter util_daq3_xcvr CONFIG.RX_CDR_CFG 0x0B000023FF10400020
+ad_ip_instance util_adxcvr util_ad9675_xcvr
+ad_ip_parameter util_ad9675_xcvr CONFIG.RX_NUM_OF_LANES [expr $RX_NUM_OF_LANES+$RX_OS_NUM_OF_LANES]#$RX_NUM_OF_LANES
+ad_ip_parameter util_ad9675_xcvr CONFIG.TX_NUM_OF_LANES $TX_NUM_OF_LANES
+ad_ip_parameter util_ad9675_xcvr CONFIG.QPLL_REFCLK_DIV 1
+ad_ip_parameter util_ad9675_xcvr CONFIG.QPLL_FBDIV_RATIO 1
+ad_ip_parameter util_ad9675_xcvr CONFIG.QPLL_FBDIV 0x30; # 20
+ad_ip_parameter util_ad9675_xcvr CONFIG.RX_OUT_DIV 1
+ad_ip_parameter util_ad9675_xcvr CONFIG.TX_OUT_DIV 1
+ad_ip_parameter util_ad9675_xcvr CONFIG.RX_DFE_LPM_CFG 0x0904
+ad_ip_parameter util_ad9675_xcvr CONFIG.RX_CDR_CFG 0x0B000023FF10400020
 
-ad_connect  $sys_cpu_resetn util_daq3_xcvr/up_rstn
-ad_connect  $sys_cpu_clk util_daq3_xcvr/up_clk
+ad_connect  $sys_cpu_resetn util_ad9675_xcvr/up_rstn
+ad_connect  $sys_cpu_clk util_ad9675_xcvr/up_clk
 
 # reference clocks & resets
 
 create_bd_port -dir I tx_ref_clk_0
 create_bd_port -dir I rx_ref_clk_0
 
-ad_xcvrpll  tx_ref_clk_0 util_daq3_xcvr/qpll_ref_clk_*
-ad_xcvrpll  rx_ref_clk_0 util_daq3_xcvr/cpll_ref_clk_*
-ad_xcvrpll  axi_ad9675_xcvr/up_pll_rst util_daq3_xcvr/up_cpll_rst_*
+ad_xcvrpll  tx_ref_clk_0 util_ad9675_xcvr/qpll_ref_clk_*
+ad_xcvrpll  rx_ref_clk_0 util_ad9675_xcvr/cpll_ref_clk_*
+ad_xcvrpll  axi_ad9675_xcvr/up_pll_rst util_ad9675_xcvr/up_cpll_rst_*
 
 # connections (adc)
 
-ad_xcvrcon  util_daq3_xcvr axi_ad9675_xcvr axi_ad9675_jesd
-ad_connect  util_daq3_xcvr/rx_out_clk_0 axi_ad9675_tpl_core/link_clk
-ad_connect  axi_ad9675_jesd/rx_sof axi_ad9675_tpl_core/link_sof
-ad_connect  axi_ad9675_jesd/rx_data_tdata axi_ad9675_tpl_core/link_data
-ad_connect  axi_ad9675_jesd/rx_data_tvalid axi_ad9675_tpl_core/link_valid
-ad_connect  axi_ad9675_tpl_core/adc_valid_0 axi_ad9675_cpack/fifo_wr_en
+ad_xcvrcon  util_ad9675_xcvr axi_ad9675_xcvr axi_ad9675_jesd
+ad_reconct  util_ad9675_xcvr/rx_out_clk_0 axi_ad9675_rx_clkgen/clk#ad_connect axi_ad9675_tpl_core/link_clk
+for {set i 0} {$i < $RX_NUM_OF_LANES} {incr i} {
+  ad_connect  $rx_link_clk util_ad9675_xcvr/rx_clk_$i
+  ad_xcvrpll  rx_ref_clk_0 util_ad9675_xcvr/cpll_ref_clk_$i
+  ad_xcvrpll  axi_ad9675_rx_xcvr/up_pll_rst util_ad9675_xcvr/up_cpll_rst_$i
+}
+ad_connect  $rx_link_clk axi_ad9675_rx_jesd/device_clk
+ad_connect  $rx_link_clk axi_ad9675_rx_jesd_rstgen/slowest_sync_clk
 
-if {$sys_zynq == 0 || $sys_zynq == 1} {
-    ad_connect  util_daq3_xcvr/rx_out_clk_0 axi_ad9675_fifo/adc_clk
-    ad_connect  axi_ad9675_jesd_rstgen/peripheral_reset axi_ad9675_fifo/adc_rst
-    ad_connect  axi_ad9675_cpack/packed_fifo_wr_en axi_ad9675_fifo/adc_wr
-    ad_connect  axi_ad9675_cpack/packed_fifo_wr_data axi_ad9675_fifo/adc_wdata
-    ad_connect  $sys_dma_clk axi_ad9675_fifo/dma_clk
-    ad_connect  $sys_dma_clk axi_ad9675_dma/s_axis_aclk
-    ad_connect  $sys_dma_resetn axi_ad9675_dma/m_dest_axi_aresetn
-    ad_connect  axi_ad9675_fifo/dma_wr axi_ad9675_dma/s_axis_valid
-    ad_connect  axi_ad9675_fifo/dma_wdata axi_ad9675_dma/s_axis_data
-    ad_connect  axi_ad9675_fifo/dma_wready axi_ad9675_dma/s_axis_ready
-    ad_connect  axi_ad9675_fifo/dma_xfer_req axi_ad9675_dma/s_axis_xfer_req
-    ad_connect  axi_ad9675_tpl_core/adc_dovf axi_ad9675_fifo/adc_wovf
+
+#if {$sys_zynq == 0 || $sys_zynq == 1} {
+#    ad_connect  util_ad9675_xcvr/rx_out_clk_0 axi_ad9675_fifo/adc_clk
+#    ad_connect  axi_ad9675_jesd_rstgen/peripheral_reset axi_ad9675_fifo/adc_rst
+#    ad_connect  axi_ad9675_cpack/packed_fifo_wr_en axi_ad9675_fifo/adc_wr
+#    ad_connect  axi_ad9675_cpack/packed_fifo_wr_data axi_ad9675_fifo/adc_wdata
+#    ad_connect  $sys_dma_clk axi_ad9675_fifo/dma_clk
+#    ad_connect  $sys_dma_clk axi_ad9675_dma/s_axis_aclk
+#    ad_connect  $sys_dma_resetn axi_ad9675_dma/m_dest_axi_aresetn
+#    ad_connect  axi_ad9675_fifo/dma_wr axi_ad9675_dma/s_axis_valid
+#    ad_connect  axi_ad9675_fifo/dma_wdata axi_ad9675_dma/s_axis_data
+#    ad_connect  axi_ad9675_fifo/dma_wready axi_ad9675_dma/s_axis_ready
+#    ad_connect  axi_ad9675_fifo/dma_xfer_req axi_ad9675_dma/s_axis_xfer_req
+#    ad_connect  axi_ad9675_tpl_core/adc_dovf axi_ad9675_fifo/adc_wovf
+#}
+
+#ad_connect  util_ad9675_xcvr/rx_out_clk_0 axi_ad9675_cpack/clk
+#ad_connect  axi_ad9675_jesd_rstgen/peripheral_reset axi_ad9675_cpack/reset
+
+#for {set i 0} {$i < $RX_NUM_OF_CONVERTERS} {incr i} {
+#  ad_connect  axi_ad9675_tpl_core/adc_enable_$i axi_ad9675_cpack/enable_$i
+#  ad_connect  axi_ad9675_tpl_core/adc_data_$i axi_ad9675_cpack/fifo_wr_data_$i
+#}
+
+
+# dma clock & reset
+ad_ip_instance proc_sys_reset sys_dma_rstgen
+ad_ip_parameter sys_dma_rstgen CONFIG.C_EXT_RST_WIDTH 1
+
+ad_connect  sys_dma_clk sys_dma_rstgen/slowest_sync_clk
+ad_connect  sys_dma_resetn sys_dma_rstgen/peripheral_aresetn
+ad_connect  sys_dma_reset sys_dma_rstgen/peripheral_reset
+# Connect JESD Link layer with Transport layer
+
+#ad_connect  axi_ad9675_jesd/rx_sof axi_ad9675_tpl_core/link_sof
+#ad_connect  axi_ad9675_jesd/rx_data_tdata axi_ad9675_tpl_core/link_data
+#ad_connect  axi_ad9675_jesd/rx_data_tvalid axi_ad9675_tpl_core/link_valid
+#ad_connect  axi_ad9675_tpl_core/adc_valid_0 axi_ad9675_cpack/fifo_wr_en
+
+ad_connect $rx_data_clk rx_ad9675_tpl_core/link_clk
+if {$RX_TPL_BYTES_PER_BEAT > $LINK_LAYER_BYTES_PER_BEAT} {
+  ad_ip_instance ad_ip_jesd204_link_dnconv rx_link_dnconverter [list\
+    NUM_LANES $RX_NUM_OF_LANES \
+    OCTETS_PER_BEAT_IN $LINK_LAYER_BYTES_PER_BEAT \
+    OCTETS_PER_BEAT_OUT $RX_TPL_BYTES_PER_BEAT \
+  ]
+  ad_connect $rx_link_clk rx_link_dnconverter/in_link_clk
+  ad_connect $rx_data_clk rx_link_dnconverter/out_link_clk
+
+  ad_connect axi_ad9675_rx_jesd/rx_data_tdata rx_link_dnconverter/in_link_data
+  ad_connect axi_ad9675_rx_jesd/rx_data_tvalid rx_link_dnconverter/in_link_valid
+  ad_connect axi_ad9675_rx_jesd/rx_sof rx_link_dnconverter/in_link_sof
+
+  ad_connect rx_link_dnconverter/out_link_data rx_ad9675_tpl_core/link_data
+  ad_connect rx_link_dnconverter/out_link_valid rx_ad9675_tpl_core/link_valid
+  ad_connect rx_link_dnconverter/out_link_sof rx_ad9675_tpl_core/link_sof
+
+} else {
+  ad_connect axi_ad9675_rx_jesd/rx_data_tdata rx_ad9675_tpl_core/link_data
+  ad_connect axi_ad9675_rx_jesd/rx_data_tvalid rx_ad9675_tpl_core/link_valid
+  ad_connect axi_ad9675_rx_jesd/rx_sof rx_ad9675_tpl_core/link_sof
 }
 
-ad_connect  util_daq3_xcvr/rx_out_clk_0 axi_ad9675_cpack/clk
-ad_connect  axi_ad9675_jesd_rstgen/peripheral_reset axi_ad9675_cpack/reset
+if {$RX_NUM_OF_CONVERTERS > 1} {
 
-for {set i 0} {$i < $RX_NUM_OF_CONVERTERS} {incr i} {
-  ad_connect  axi_ad9675_tpl_core/adc_enable_$i axi_ad9675_cpack/enable_$i
-  ad_connect  axi_ad9675_tpl_core/adc_data_$i axi_ad9675_cpack/fifo_wr_data_$i
+  #connect DMA to TPL through cpack
+  ad_ip_instance util_cpack util_ad9675_rx_cpack
+  ad_ip_parameter util_ad9675_rx_cpack CONFIG.CHANNEL_DATA_WIDTH [expr $RX_SAMPLE_WIDTH*$RX_SAMPLES_PER_CHANNEL]
+  ad_ip_parameter util_ad9675_rx_cpack CONFIG.NUM_OF_CHANNELS $RX_NUM_OF_CONVERTERS
+
+  ad_connect  $rx_data_clk util_ad9675_rx_cpack/adc_clk
+  ad_connect  axi_ad9675_rx_jesd_rstgen/peripheral_reset util_ad9675_rx_cpack/adc_rst
+
+  for {set i 0} {$i < $RX_NUM_OF_CONVERTERS} {incr i} {
+
+    ad_connect rx_ad9675_tpl_core/adc_data_$i util_ad9675_rx_cpack/adc_data_$i
+    ad_connect rx_ad9675_tpl_core/adc_enable_$i util_ad9675_rx_cpack/adc_enable_$i
+    ad_connect rx_ad9675_tpl_core/adc_valid_$i util_ad9675_rx_cpack/adc_valid_$i
+  }
+
+  ad_connect  util_ad9675_rx_cpack/adc_valid axi_ad9675_rx_dma/fifo_wr_en
+  ad_connect  util_ad9675_rx_cpack/adc_sync axi_ad9675_rx_dma/fifo_wr_sync
+  ad_connect  util_ad9675_rx_cpack/adc_data axi_ad9675_rx_dma/fifo_wr_din
+
+} else {
+  #connect DMA to TPL directly
+  ad_connect rx_ad9675_tpl_core/adc_valid_0 axi_ad9675_rx_dma/fifo_wr_en
+  ad_connect rx_ad9675_tpl_core/adc_data_0 axi_ad9675_rx_dma/fifo_wr_din
 }
+ad_connect  axi_ad9675_rx_dma/fifo_wr_overflow rx_ad9675_tpl_core/adc_dovf
+ad_connect  $rx_data_clk axi_ad9675_rx_dma/fifo_wr_clk
+ad_connect  sys_dma_resetn axi_ad9675_rx_dma/m_dest_axi_aresetn
 
 # interconnect (cpu)
 
